58b82265933afb2e2fec439a971946f956a3fdf1
pmac1687 <58713706+pmac1687@users.noreply.github.com>
2020-05-28 10:37:26 -0700

    Update settings.py

--- jordanspage/settings.py
+++ jordanspage/settings.py
@@ -123,11 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 
 #handle media files on local path
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 MEDIAFILES_DIRS = [(os.path.join(BASE_DIR, 'media'))]
